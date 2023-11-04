f347feff15434562372e54437b7d9ec74b105520
anjali ahlawat <anjaliahlawat45@gmail.com>
2023-11-04 13:09:04 +0530

    added program

---
+++ patterns/prt6.java
@@ -0,0 +1,20 @@
+public class prt6 
+{
+    public static void main(String[] args)
+    {
+        for(int i=5;i>=1;i--)
+        {
+            int count=1;
+      
+        for(int j=1;j<=i;j++)
+        { 
+
+            System.out.print(count);
+            count++;
+        
+        }
+        System.out.println();
+
+        }
+    }
+}
