f15d61ed7c83853386470b6186634fc42d35149e
anjali ahlawat <anjaliahlawat45@gmail.com>
2023-11-06 19:52:41 +0530

    added program

--- patterns/prt16.java
+++ patterns/prt16.java
@@ -2,16 +2,18 @@ public class prt16
 {
     public static void main(String[] args) 
     {
-        
+        char ch='A';
         for(int i=1;i<=5;i++)
         {    
-            char ch='A';
+            
             for(int j=1;j<=i;j++)
             { 
-                System.out.print(ch+i);
+                 System.out.print(ch);
         
             }
+            
             System.out.println();
+            ch++;
         }
     }
 }
