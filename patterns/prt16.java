cf1c7302a0fc4870daf13a5e9274765098ee560a
anjali ahlawat <anjaliahlawat45@gmail.com>
2023-11-06 19:34:56 +0530

    added program

---
+++ patterns/prt16.java
@@ -0,0 +1,17 @@
+public class prt16 
+{
+    public static void main(String[] args) 
+    {
+        
+        for(int i=1;i<=5;i++)
+        {    
+            char ch='A';
+            for(int j=1;j<=i;j++)
+            { 
+                System.out.print(ch+i);
+        
+            }
+            System.out.println();
+        }
+    }
+}
