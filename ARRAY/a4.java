ac9f1170c2bb19d963642506c13fcc08c9a0b603
anjali ahlawat <anjaliahlawat45@gmail.com>
2023-11-12 09:51:00 +0530

    added program

---
+++ ARRAY/a4.java
@@ -0,0 +1,32 @@
+//Check if an Array is Sorted
+
+public class a4 
+{
+    public static void  main(String[] args) 
+    {
+    
+        int []a={1,2,3,2};
+        boolean  result = search(a);
+        System.out.println(result);
+    }
+     public static boolean search(int[] a){
+        int n=a.length;
+        for(int i=1;i<n;i++)
+        {
+               if(a[i-1]<=a[i])
+               {
+                  
+               }
+               else
+               {
+                  return false;
+               }
+        }
+        return true;
+    }
+
+
+}
+
+
+
