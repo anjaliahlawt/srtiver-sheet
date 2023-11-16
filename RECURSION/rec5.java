662bd4b3b91551a486bbb565331fd5089724bfbd
anjali ahlawat <anjaliahlawat45@gmail.com>
2023-11-16 15:54:55 +0530

    added program

---
+++ RECURSION/rec5.java
@@ -0,0 +1,21 @@
+//Sum of first N Natural Numbers
+
+package RECURSION;
+
+public class rec5
+{
+    public static void main(String[] args)
+    {
+      int n=7;
+       int c=sum(n);  
+      System.out.println(c);
+    }
+    static int sum(int n)
+    {
+        if(n==0)
+        {
+            return 0;
+        }
+        return n+ sum(n-1);
+    }
+}
