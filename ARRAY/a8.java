ba3bc2ce4808132c541061b289ddfd9d45571162
anjali ahlawat <anjaliahlawat45@gmail.com>
2023-11-16 17:01:36 +0530

    added program

---
+++ ARRAY/a8.java
@@ -0,0 +1,15 @@
+//revrse an array
+
+public class a8 
+{
+    public static void main(String[] args) 
+    {
+       int[]arr={1,2,3,4,6};
+       int n=arr.length-1;
+      for(int j=n;j>=0;j--)
+      {
+         System.out.print(arr[j]+" ");
+      }
+} 
+}
+    
---
+++ RECURSION/rec7.java
@@ -0,0 +1,13 @@
+//package RECURSION;
+import java.util.*;
+public class rec7 
+{public static void main(String[] args) {
+ 
+
+
+    int[]arr={1,2,3,4,6};
+    int n=arr.length-1;
+    for(int j=n;j>=0;j--){
+      System.out.print(arr[j]+" ");
+    }
+}}
