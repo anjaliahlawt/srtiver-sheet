c563115bcaa912feb663b39391fd1c255cd162eb
anjali ahlawat <anjaliahlawat45@gmail.com>
2023-11-16 15:19:14 +0530

    added program

---
+++ RECURSION/rec3.java
@@ -0,0 +1,22 @@
+//Print 1 to N using Recursion
+
+package RECURSION;
+
+public class rec3
+{
+    public static void main(String[] args)
+    {
+        int n=6;
+        number(1,n);
+    }
+    static void number(int i,int n)
+    {
+        if(i>n)
+        {
+            return;
+        }
+        System.out.println(i);
+        
+        number(i+1,n);
+    }
+}
