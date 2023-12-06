8a77e9d7ec0da3043b8ce31c485c52d6ea1d1f6e
anjali ahlawat <anjaliahlawat45@gmail.com>
2023-12-06 10:48:52 +0530

    added program

--- ARRAY/a11.java
+++ ARRAY/a11.java
@@ -5,8 +5,7 @@ public class a11
     public static void main(String[] args) 
     {
         int arr[]={1,2,3,4,5};
-       
-       int n=arr.length-1;
+        int n=arr.length-1;
         int temp=arr[0];
        for(int i=1;i<=n;i++)
        {
