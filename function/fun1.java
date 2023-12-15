97bd81a3d7155e925d5cb81fc3824a9854630fbe
anjali ahlawat <anjaliahlawat45@gmail.com>
2023-12-15 17:00:18 +0530

    added program

---
+++ function/fun1.java
@@ -0,0 +1,17 @@
+package function;
+
+public class fun1 
+{
+    public static void main(String[] args) 
+    {
+        int a=4,b=5;
+         int ans=addTwoNumber(a,b);
+         System.out.println(ans);
+    }
+     static int addTwoNumber(int a,int b)
+    {
+         int c=a+b;
+         return c;
+    }
+    
+}
