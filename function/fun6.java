cdb1da1247d3939794147b99bd6d57b037b095d8
anjali ahlawat <anjaliahlawat45@gmail.com>
2023-12-16 12:04:10 +0530

    added program

---
+++ function/fun6.java
@@ -0,0 +1,21 @@
+
+//Write a function that takes in age as input and returns if that person is eligible to vote or not. A person of age > 18 is eligible to vote.
+
+package function;
+
+public class fun6
+{
+   public static void main(String[] args)
+    {
+        int age =18;
+        if(age>=18)
+        {
+           System.out.println("is elgible to vote");
+        }
+        else
+        {
+            System.out.println("is not eligible for vote");
+        }
+          
+   } 
+}
