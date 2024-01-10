b8dd783f0771fdc975f4ce1d9d4c17608f4fe9f8
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-01-10 19:50:33 +0530

    added program

---
+++ ARRAY/a15.java
@@ -0,0 +1,18 @@
+
+//linear search
+public class a15 
+{
+    public static void main(String[] args) 
+    {
+        int []arr={1,2,3,4,5};
+        int n=arr.length;
+        int target=1;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==target)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
