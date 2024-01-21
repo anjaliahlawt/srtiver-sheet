7f40a31a68ce6164b31f0bf93ec69ace28549db3
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-01-21 23:50:48 +0530

    added program

---
+++ sorting/selectionsort.java
@@ -0,0 +1,38 @@
+
+//selsction sort
+
+package sorting;
+public class selectionsort
+{
+    public static void main(String[] args) 
+    {
+        int []arr={13,46,24,52,20,9};
+        int n =arr.length;
+        System.out.println("before  selection sorting");
+        for(int k=0;k<n;k++)
+        {
+            System.out.print(arr[k]+" ");
+        }
+       System.out.println();
+        for(int i=0;i<n-1;i++)
+        {
+            int min=i;
+            for(int j =i+1;j<n;j++)
+            {
+                if(arr[j]<arr[min])
+                {
+                    min=j;
+                }
+            }
+            int temp=arr[min];
+            arr[min]=arr[i];
+            arr[i]=temp;
+        }
+     System.out.println("after selction sorting");
+     for(int k=0;k<n;k++)
+     {
+        System.out.print(arr[k]+" ");
+     }
+     System.out.println();
+    }
+}
