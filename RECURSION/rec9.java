c822ca44eeb0a749b93ccaf0cc0641b5a19b6b46
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-04-04 15:15:53 +0530

    added program

--- RECURSION/rec9.java
+++ RECURSION/rec9.java
@@ -1,21 +1,17 @@
 //fibonacii series print using recursion
-public class rec9
- {  static int a=0,b=1,c=0;
-    public static void main(String[] args) 
-    {  
-        int n=5;
-        System.out.print(a+""+b);
-        fibonnaci(n-2);
-    }
-    static void fibonnaci(int n)
-    { 
-       if(n>0)
-       {
-          c=a+b;
-          a=b;
-          b=c;
-          System.out.print(c);
-          fibonnaci(n-1);
-       }
-    }
+class rec9
+{
+   public static void main(String[] args) 
+   {
+      int result=fibonacci(5);
+      System.out.println(result);
+      
+   }
+
+   static int fibonacci(int n )
+   {
+      if(n==0||n==1)
+      return n ;
+      return fibonacci(n-1)+fibonacci(n-2);
+   }
 }
---
+++ sorting/bsur1.java
@@ -0,0 +1,34 @@
+//bubble sort using recusrion
+
+class bsur1
+{
+    public static void main (String[] args)
+    {
+        int arr[]={13,25,9,7,12};
+        int n =arr.length;
+        bubblesort(arr,n);
+        System.out.println("after bubble sort");
+        for(int i=0;i<n;i++)
+        {
+           System.out.print(arr[i]+" ");
+        }
+
+    }
+    static void bubblesort(int[]arr,int n)
+    {
+        if(n==1)
+        return;
+        for(int j=0;j<=n-2;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                int temp=arr[j];
+                arr[j]=arr[j+1]; 
+                arr[j+1]=temp;
+
+            }
+        }
+        bubblesort(arr,n-1);
+    }
+
+}
