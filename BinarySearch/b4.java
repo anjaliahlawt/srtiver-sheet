07070ee803324316ce68683a42145a20afbd8622
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-04-05 15:38:51 +0530

    added program

--- BinarySearch/b4.java
+++ BinarySearch/b4.java
@@ -1,4 +1,6 @@
-public class b4 
+//Search Insert Position
+
+/*public class b4 
 {
     public static void main(String[] args) 
     {
@@ -22,4 +24,36 @@ public class b4
         return n;
     }
     
+}
+*/
+class b4
+{
+    public static void main(String[] args) 
+    {
+        int arr[]={1,2,2,4,7};
+        int n =arr.length;
+        int x=6;
+        int result=index(arr,n,x);
+        System.out.println(result);
+
+    }
+    static int index(int[]arr,int n,int x)
+    {
+        int start=0;int end=n-1;
+        int ans=n;
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+            if(arr[mid]>=x)
+            {
+              ans=mid;
+              end=mid-1;
+            }
+            else
+            {
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
 }
