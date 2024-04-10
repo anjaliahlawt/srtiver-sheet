dd53a4b1d2c918271dabe0e39292d09d8cd503d4
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-04-10 22:58:31 +0530

    added program

--- BinarySearch/b10.java
+++ BinarySearch/b10.java
@@ -19,14 +19,15 @@ public class b10
     }
 }
 */
-
+/* 
 import java.util.*;
 
 class b10{
     public static int findMin(int []arr) {
         int n = arr.length; 
         int mini = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
           
             mini = Math.min(mini, arr[i]);
         }
@@ -40,3 +41,35 @@ class b10{
     }
 }
 
+*/
+import java.util.*;
+class b10
+{
+    public static void main(String[] args) 
+    {
+        int[]arr={5,6,7,8,1,2,3};
+        int n =arr.length;
+        int ans=min(arr,n);
+        System.out.println(ans);
+    }
+    static int min(int[]arr,int n)
+    {
+        int ans=Integer.MAX_VALUE;
+        int start=0;int end=n-1;
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+            if(arr[start]<=arr[mid])
+            {
+                ans=Math.min(ans,arr[start]);
+                start=mid+1;
+            }
+            else
+            {
+                ans=Math.min(ans,arr[mid]);
+                end=mid-1;
+            }
+        }
+        return ans;
+    }
+}
---
+++ BinarySearch/b11.java
@@ -0,0 +1,81 @@
+
+//Find out how many times the array has been rotated
+
+/* 
+public class b11 
+{
+    public static void main(String[] args)
+    {
+        int[]arr={4,5,6,7,0,1,2,3};
+        int n=arr.length;
+        int ans=rotate(arr,n);
+        System.out.println(ans);
+        
+    }
+    static int rotate(int[]arr,int n)
+    {
+        int ans=Integer.MAX_VALUE;
+        int index=-1;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<ans)
+            {
+                ans=arr[i];
+                index=i;
+            }
+        }
+        return index;
+
+    }
+    
+}
+*/
+class b11
+{
+    public static void main(String[] args) 
+    {
+        int[]arr={3,4,5,0,1,2};
+        int n =arr.length;
+        int ans=rotate(arr,n);
+        System.out.println(ans);
+    }
+    static int rotate(int[]arr,int n)
+    {
+        int ans=Integer.MAX_VALUE;
+        int start=0;int end=n-1;
+        int index=-1;
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+            if(arr[start]<=arr[end])
+            {
+                if(arr[start]<ans)
+                {
+                    index=start;
+                    ans=arr[start];
+                    break;
+                }
+            }
+            if(arr[start]<arr[mid])
+            {
+                if(arr[start]<ans)
+                {
+                    index=start;
+                    ans=arr[start];
+                }
+                start=mid+1;
+            }
+            else
+            {
+                if(arr[mid]<ans)
+                {
+                    index=mid;
+                    ans=arr[mid];
+
+                }
+                end=mid-1;
+            }
+        }
+        return index;
+    }
+}
