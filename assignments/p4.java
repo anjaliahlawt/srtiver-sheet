c200a5b0adc6663d9807a7b146888b27b57dd0ce
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-04-12 16:44:54 +0530

    added program

--- assignments/p4.java
+++ assignments/p4.java
@@ -1,5 +1,5 @@
 // write a program to convert binary number to decimal number
-
+/* 
 public class p4 
 {
     public static void main(String[] args) 
@@ -9,3 +9,27 @@ public class p4
         System.out.println("the decimal number of "+binary+""+"="+decimal);
     }
 }
+*/
+ class p4
+ {
+    public static void main(String[] args) 
+    {
+        long num=110110111;
+        int d=convertdecimal(num);
+        System.out.println(d);
+
+    }
+    static int convertdecimal(long num)
+    {
+        int dn=0;int i=0;
+        long rem;
+        while(num!=0)
+        {
+            rem=num%10;
+            num=num/10;
+            dn+=rem*Math.pow(2,i);
+            ++i;
+        }
+        return dn;
+    }
+ }
