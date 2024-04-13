1415cde707d0a8a6008073deac5c7b81b83eed2d
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-04-13 11:54:32 +0530

    added program

---
+++ BinarySearch/b12.java
@@ -0,0 +1,44 @@
+public class b12
+{
+   public static void main(String[] args)
+   {
+     int[]arr={1,1,2,2,3,4,4,4};
+     int n =arr.length;
+      int ans=sorted(arr,n);
+      System.out.println(ans);
+   } 
+   static int sorted(int[]arr,int n)
+   {
+     if(n==1)
+     {
+        return arr[0];
+     }
+     for(int i=0;i<n;i++)
+     {
+        if(i==0)
+        {
+            if(arr[i]!=arr[i+1])
+            {
+                return arr[i];
+            }
+        }
+        else if(i==n-1)
+        {
+            if(arr[i]!=arr[i-1])
+            {
+                return arr[i];
+            }
+        }
+        else
+        {
+            if(arr[i]!=arr[i+1]&&arr[i]!=arr[i-1])
+            {
+                return arr[i];
+            }
+        }
+     }
+     return 0;
+   }
+   
+   
+}
