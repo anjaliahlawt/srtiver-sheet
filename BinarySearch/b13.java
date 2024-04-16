0ecdadf7b8497f06dc25a71aae6f3c67721641c6
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-04-16 23:01:10 +0530

    added program

--- BinarySearch/b13.java
+++ BinarySearch/b13.java
@@ -1,3 +1,5 @@
+//Peak element in Array
+/* 
 public class b13
 {
    public static void main(String[] args) 
@@ -21,5 +23,38 @@ public class b13
         }
       
  } 
-      
+  */
+  
+  class b13
+  {
+   public static void main(String[] args) 
+   {
+      int[]arr={1, 2, 3, 4, 5, 6, 7, 8, 5, 1};
+      int n=arr.length;
+      int ans=peak(arr,n);
+      System.out.println(ans);
+   }
+   static int peak(int[]arr,int n)
+   {
+      if (n == 1) return 0;
+      if (arr[0] > arr[1]) return 0;
+      if (arr[n - 1] > arr[n - 2]) return n - 1;
+      int start=1;int end=n-2;
+      while(start<=end)
+      {
 
+       int mid=(start+end)/2;
+      
+       if(arr[mid-1]<arr[mid]&&arr[mid]>arr[mid+1])
+       {
+         return mid;
+       }
+       if(arr[mid]>arr[mid-1])
+       {
+         start=mid+1;
+       }
+   }
+   return -1;
+  }
+
+  }
